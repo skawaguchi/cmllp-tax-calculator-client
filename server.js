3be6b2d980568c7c46ca71d27bc030b98a5f2fe3
Stephen Kawaguchi <stephen.kawaguchi@gmail.com>
2016-06-01 09:20:34 -0400

    fix: Port number reference was wrong

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const vision = require('vision');
 
 const server = new Hapi.Server();
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 server.connection({
     port
