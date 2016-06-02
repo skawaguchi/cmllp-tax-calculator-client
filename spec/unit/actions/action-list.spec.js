f1251487d9ff914d6c0271f6fe7f76c812f3ee05
Stephen Kawaguchi <stephen.kawaguchi@gmail.com>
2016-06-02 00:58:41 -0400

    feat: Add more calculation actions

--- spec/unit/actions/action-list.spec.js
+++ spec/unit/actions/action-list.spec.js
@@ -4,6 +4,8 @@ import * as actions from '../../../src/actions/action-list';
 
 test('# Action List', (t) => {
 
+    t.equal(actions.CALCULATION_LOAD_ERROR, 'CALCULATION_LOAD_ERROR', 'should have a calculation load error action');
+    t.equal(actions.CALCULATION_LOAD_START, 'CALCULATION_LOAD_START', 'should have a calculation loads start action');
     t.equal(actions.CALCULATION_LOADED, 'CALCULATION_LOADED', 'should have a calculation loaded action');
 
     t.end();
