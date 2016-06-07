c943e327158ee95b1275f89104588ce8ec94283c
Stephen Kawaguchi <stephen.kawaguchi@gmail.com>
2016-06-07 09:29:17 -0400

    feat: Added action type for normal income changed

--- spec/unit/actions/action-list.spec.js
+++ spec/unit/actions/action-list.spec.js
@@ -1,12 +1,18 @@
 import test from 'tape';
 
-import * as actions from '../../../src/actions/action-list';
+import {
+    CALCULATION_LOAD_ERROR,
+    CALCULATION_LOAD_START,
+    CALCULATION_LOADED,
+    NORMAL_INCOME_CHANGED
+} from '../../../src/actions/action-list';
 
 test('# Action List', (t) => {
 
-    t.equal(actions.CALCULATION_LOAD_ERROR, 'CALCULATION_LOAD_ERROR', 'should have a calculation load error action');
-    t.equal(actions.CALCULATION_LOAD_START, 'CALCULATION_LOAD_START', 'should have a calculation loads start action');
-    t.equal(actions.CALCULATION_LOADED, 'CALCULATION_LOADED', 'should have a calculation loaded action');
+    t.equal(CALCULATION_LOAD_ERROR, 'CALCULATION_LOAD_ERROR', 'should have a calculation load error action');
+    t.equal(CALCULATION_LOAD_START, 'CALCULATION_LOAD_START', 'should have a calculation loads start action');
+    t.equal(CALCULATION_LOADED, 'CALCULATION_LOADED', 'should have a calculation loaded action');
+    t.equal(NORMAL_INCOME_CHANGED, 'NORMAL_INCOME_CHANGED', 'should have an action type for handling normal income changes');
 
     t.end();
 });
