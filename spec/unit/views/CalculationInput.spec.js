26f261b299ae5454ed611a9ffc4b55bd82267067
Stephen Kawaguchi <stephen.kawaguchi@gmail.com>
2016-06-08 09:09:43 -0400

    feat: Added more features to CalculatorInput
    - Accepts a value
    - Translated labelKey into <FormattedMessage/>

--- spec/unit/views/CalculationInput.spec.js
+++ spec/unit/views/CalculationInput.spec.js
@@ -18,24 +18,37 @@ function teardown() {
     sandbox.restore();
 }
 
+/* eslint-disable max-statements */
+
 test('# CalculationInput > Given the control renders', (t) => {
 
     setup();
 
-    const fakeLabel = chance.string();
+    const fakeLabelKey = chance.hash();
     const fakeInputID = chance.hash({length: 5});
+    const fakeClassName = chance.hash({length: 5});
+    const fakeValue = chance.floating({max: 10000});
     const fakeChangeHandler = () => {};
 
     const component = shallow(<CalculationInput
             changeHandler={fakeChangeHandler}
+            className={fakeClassName}
             inputID={fakeInputID}
-            label={fakeLabel}
+            inputValue={fakeValue}
+            labelKey={fakeLabelKey}
         />);
 
     t.equal(component.type(), 'div', 'should be a <div/>');
-    t.equal(component.find('label').text(), fakeLabel, 'should display the label');
+
+    t.equal(component.find('label FormattedMessage').prop('id'), fakeLabelKey, 'should have a localized label');
+
     t.equal(component.unrendered.props.changeHandler, fakeChangeHandler, 'should be passed a change handler');
+    t.equal(component.prop('className'), `calculation-input ${fakeClassName}`, 'should be passed a className');
     t.equal(component.unrendered.props.inputID, fakeInputID, 'should be assigned a unique identifier so that the change callback can know which input was pressed');
+    t.equal(component.unrendered.props.labelKey, fakeLabelKey, 'should have a label key that matches up to a translated value');
+
+    t.equal(component.find('input').prop('type'), 'number', 'should have a numeric input');
+    t.equal(component.find('input').prop('value'), fakeValue, 'should have a value');
 
     teardown();
 
@@ -54,6 +67,7 @@ test('# CalculationInput > when the input is changed', (t) => {
     const component = shallow(
         <CalculationInput
             changeHandler={fakeChangeHandler}
+            className='fake-class-name'
             inputID={fakeID}
         />
     );
--- src/views/CalculationInput.js
+++ src/views/CalculationInput.js
@@ -1,24 +1,33 @@
-import React, {PropTypes, Component} from 'react';
+import React, {PropTypes} from 'react';
+import {FormattedMessage} from 'react-intl';
 
 function inputChanged(id, changeHandler, event) {
     changeHandler(id, event.target.value);
 }
 
-class CalculationInput extends Component {
-    render() {
-        return (
-            <div className='calculation-input'>
-                <label>{this.props.label}</label>
-                <input onChange={inputChanged.bind(null, this.props.inputID, this.props.changeHandler)} />
-            </div>
-        );
-    }
+function getClassName(className) {
+    return `calculation-input ${className}`;
+}
+
+function CalculationInput(props) {
+    return (
+        <div className={getClassName(props.className)}>
+            <label><FormattedMessage id={props.labelKey}/></label>
+            <input
+                onChange={inputChanged.bind(null, props.inputID, props.changeHandler)}
+                type='number'
+                value={props.inputValue}
+            />
+        </div>
+    );
 }
 
 CalculationInput.propTypes = {
     changeHandler: PropTypes.func.isRequired,
+    className: PropTypes.string.isRequired,
     inputID: PropTypes.string.isRequired,
-    label: PropTypes.string
+    inputValue: PropTypes.number.isRequired,
+    labelKey: PropTypes.string
 };
 
 export default CalculationInput;
