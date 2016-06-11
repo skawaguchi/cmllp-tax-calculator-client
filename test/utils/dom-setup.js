ffff4a3a3872ef4cd831f68cc5e30067f4d38d4f
Stephen Kawaguchi <stephen.kawaguchi@gmail.com>
2016-06-11 16:12:48 -0400

    feat: Added react-dom in case its needed in tests

---
+++ test/utils/dom-setup.js
@@ -0,0 +1,24 @@
+// http://jaketrent.com/post/testing-react-with-jsdom/
+import jsdom from 'jsdom';
+
+const doc = jsdom.jsdom('<!DOCTYPE html><html><body></body></html>');
+const win = doc.defaultView;
+
+global.document = doc;
+global.window = win;
+
+propagateToGlobal(win);
+
+// from mocha-jsdom https://github.com/rstacruz/mocha-jsdom/blob/master/index.js#L80
+function propagateToGlobal (window) {
+    for (let key in window) {
+        if (!window.hasOwnProperty(key)) {
+            continue;
+        }
+        if (key in global) {
+            continue;
+        }
+
+        global[key] = window[key];
+    }
+}
