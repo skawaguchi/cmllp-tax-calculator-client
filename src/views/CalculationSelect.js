d41ad1a55c68b8ef0ef7828c61cbdd83a72bbcb7
Stephen Kawaguchi <stephen.kawaguchi@gmail.com>
2016-06-11 20:42:54 -0400

    feat: Added the calculation select control

---
+++ src/views/CalculationSelect.js
@@ -0,0 +1,51 @@
+import React, {PropTypes} from 'react';
+import {intlShape, FormattedMessage} from 'react-intl';
+
+function getOptions(intl, options) {
+    return options.map((option, index) => (
+            <option
+                key={`option-${ index }`}
+                value={option.value}
+            >
+                {intl.formatMessage({id: option.labelKey})}
+            </option>
+        )
+    );
+}
+
+function selectChanged(props, event) {
+    props.changeHandler(event, event.target.value);
+}
+
+function CalculationSelect(props, context) {
+    return (
+        <div className='calculation-select'>
+            <label
+                htmlFor={props.selectID}
+            >
+                <FormattedMessage id={props.labelKey}/>
+            </label>
+            <select
+                id={props.selectID}
+                onChange={selectChanged.bind(null, props)}
+            >
+                {getOptions(context.intl, props.options)}
+            </select>
+        </div>
+    );
+}
+
+CalculationSelect.displayName = 'CalculationSelect';
+
+CalculationSelect.propTypes = {
+    changeHandler: PropTypes.func.isRequired,
+    labelKey: PropTypes.string.isRequired,
+    options: PropTypes.array.isRequired,
+    selectID: PropTypes.string.isRequired
+};
+
+CalculationSelect.contextTypes = {
+    intl: intlShape
+};
+
+export default CalculationSelect;
