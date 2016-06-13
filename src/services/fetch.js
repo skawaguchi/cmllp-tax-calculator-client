644d239ef2cea77cb9800b6693afc076b166ae85
Stephen Kawaguchi <stephen.kawaguchi@gmail.com>
2016-06-13 14:41:52 -0400

    fix: Fixed fetch server tests

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -1,17 +1,19 @@
 import 'isomorphic-fetch';
 
-export function fetch(url, options = {}) {
+export function fetch(url, options) {
     const root = global !== undefined ? global : window !== undefined ? window : this;
 
     const combinedOptions = Object.assign({}, options, {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
     });
 
     return root.fetch(url, combinedOptions)
         .then((response) => {
             if(!response.ok) {
-                Promise.reject(response);
+                return Promise.reject(response);
             }
             return response;
         });
--- test/unit/services/fetch.spec.js
+++ test/unit/services/fetch.spec.js
@@ -1,6 +1,6 @@
 import Chance from 'chance';
 import sinon from 'sinon';
-import test from 'tape';
+import test from 'blue-tape';
 import * as fetchService from '../../../src/services/fetch';
 
 let sandbox,
@@ -18,61 +18,59 @@ function teardown() {
     sandbox.restore();
 }
 
-test('# Fetch Service > fetch()', (t) => {
+const suiteName = '# Fetch Service > fetch() > ';
 
-    t.test('> Request', (st) => {
-        setup();
+test(`${suiteName} Request`, (t) => {
+    setup();
 
-        const fakeURL = chance.url();
-        const fakeResponse = {ok: true};
-        const fakeOption = {someFakeOption: true};
+    const fakeURL = chance.url();
+    const fakeResponse = {ok: true};
+    const fakeOption = {someFakeOption: true};
 
-        fetchMock.returns(Promise.resolve(fakeResponse));
+    fetchMock.returns(Promise.resolve(fakeResponse));
 
-        fetchService.fetch(fakeURL, fakeOption).then(() => {});
+    fetchService.fetch(fakeURL, fakeOption).then(() => {});
 
-        st.equal(fetchMock.firstCall.args[0], fakeURL, 'should request a URL.');
-        st.equal(fetchMock.firstCall.args[1].Accept, 'application/json', 'should request to get JSON.');
-        st.equal(fetchMock.firstCall.args[1]['Content-Type'], 'application/json', 'should request to get JSON.');
-        st.equal(fetchMock.firstCall.args[1].someFakeOption, true, 'should pass on additional options.');
+    t.equal(fetchMock.firstCall.args[0], fakeURL, 'should request a URL.');
+    t.equal(fetchMock.firstCall.args[1].headers.Accept, 'application/json', 'should request with the accept json header');
+    t.equal(fetchMock.firstCall.args[1].headers['Content-Type'], 'application/json', 'should request with the content-type json header.');
+    t.equal(fetchMock.firstCall.args[1].someFakeOption, true, 'should pass on additional options.');
 
-        teardown();
-
-        st.end();
-    });
-
-    t.test('> Response Success', (st) => {
-        setup();
-
-        const fakeResponse = {ok: true};
-
-        fetchMock.returns(Promise.resolve(fakeResponse));
-
-        fetchService.fetch().then((response) => {
-            st.equal(response, fakeResponse, 'should pass the successful fetch back to the consumer');
-        });
-
-        teardown();
-
-        st.end();
-    });
-
-    t.test('> Response Error', (st) => {
-        setup();
-
-        const fakeResponse = {ok: false};
-
-        fetchMock.returns(Promise.resolve(fakeResponse));
-
-        fetchService.fetch()
-            .catch((response) => {
-                st.equal(response, fakeResponse, 'should pass the failure back to the consumer');
-            });
-
-        teardown();
-
-        st.end();
-    });
+    teardown();
 
     t.end();
+});
+
+test(`${suiteName} Response Success`, (t) => {
+    setup();
+
+    const fakeResponse = {ok: true};
+
+    fetchMock.returns(Promise.resolve(fakeResponse));
+
+    fetchService.fetch().then((response) => {
+        t.equal(response, fakeResponse, 'should pass the successful fetch back to the consumer');
+
+        teardown();
+
+        t.end();
+    });
+
+});
+
+
+test(`${suiteName} Response Error`, (t) => {
+
+    setup();
+
+    const fakeResponse = {ok: false};
+
+    fetchMock.returns(Promise.resolve(fakeResponse));
+    fetchService.fetch()
+        .catch((response) => {
+            t.equal(response, fakeResponse, 'should pass the failure back to the consumer');
+            teardown();
+
+            t.end();
+        });
 });
