9982bd13f5c80215dc9fb18cfbd492ae1a7bc776
Stephen Kawaguchi <stephen.kawaguchi@gmail.com>
2016-06-17 17:13:30 -0400

    chore: Removed unused service code

--- src/services/ip-location.js
+++ src/services/ip-location.js
@@ -1,20 +1,5 @@
 import {fetch as fetchCall} from './fetch';
 
-const canadaCountryCode = 'CA';
-
-export function processLocation(body) {
-    if (body.country === canadaCountryCode) {
-        return {
-            country: body.country,
-            province: body.region
-        };
-    }
-    return {
-        error: `The country returned was ${ body.country }. To find the Canadian province, you must be in Canada.`
-    };
-
-}
-
 export function getProvince() {
     const resourceURL = 'http://ipinfo.io';
 
